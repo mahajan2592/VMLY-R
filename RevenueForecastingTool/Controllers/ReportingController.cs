3cecb9c82fe43496603675548600c21838be9253
Neha Mahajan <neha.mahajan@cyclingsportsgroup.com>
2022-03-20 20:26:28 +1100

    Added Reporting Feature

---
+++ RevenueForecastingTool/Controllers/ReportingController.cs
@@ -0,0 +1,47 @@
+﻿using RevenueForecastingTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RevenueForecastingTool.Controllers
+{
+    public class ReportingController : Controller
+    {
+        private DB_Entities _db = new DB_Entities();
+        // GET: Reporting
+        public ActionResult Index()
+        {
+            List<decimal> RevenueArray = new List<decimal>();
+            using (var context = new DB_Entities())
+            {
+                var projectData = context.Projects.ToList();
+                //var RevenueArray = projectData.Select(x => x.Revenue).ToArray();
+                string RevenueMonth = string.Empty;
+               
+                foreach (var data in projectData)
+                {
+                    switch (data.Month)
+                    {
+                        case "January":
+                        case "February":
+                        case "March":
+                        case "April":
+                        case "May":
+                        case "June":
+                            RevenueArray.Add(projectData.Where(x=>x.Month==data.Month).Select(r =>r.Revenue).FirstOrDefault());
+                            // code block
+                            break;
+                        default:
+                            // code block
+                            break;
+                    }
+                }
+
+            }
+            ViewBag.RevenueArray = RevenueArray.ToArray();
+            return View();
+        }
+    }
+}
---
+++ RevenueForecastingTool/Scripts/Reporting/Reporting.js
@@ -0,0 +1,23 @@
+﻿
+
+var yValues = revenueArray;
+var xValues = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: true}
+  }
+});
+
+
